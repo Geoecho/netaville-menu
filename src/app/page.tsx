b583d08a3af4272f70436d68827ef1e8658f54f3
Geoecho <hbristik@gmail.com>
2026-05-10 23:51:39 +0200

    chore: swap sections order in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,8 @@ import PromotionsSection from "@/components/PromotionsSection";
 export default function Home() {
   return (
     <main className="flex h-screen w-screen flex-col overflow-hidden">
-      <TopSection />
       <PromotionsSection />
-      
-      
+      <TopSection />
     </main>
   );
 }
